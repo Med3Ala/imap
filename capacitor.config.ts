6a03c0ae114511b96ca3512c73ed7a7559949406
ala.mouhamed <ala.mouhamed@tunav.com>
2024-08-12 22:14:21 +0100

    adding capacitor & android, for mobile deployment

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'test.imap.app',
+  appName: 'imap',
+  webDir: 'dist/imap',
+};
+
+export default config;
